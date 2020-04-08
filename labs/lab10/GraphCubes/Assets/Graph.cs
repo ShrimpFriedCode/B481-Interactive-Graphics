0816791b6365eed987d36c36b3f0b93e4ed1a6ab
Ethan Anderson <etmander@indiana.edu>
2020-04-08 01:58:43 -0400

    Lab 10, finished 4/7/2020. Late by ~2 hours due to upload speeds.

---
+++ labs/lab10/GraphCubes/Assets/Graph.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+	public Transform pointPrefab;
+    [Range(10, 100)]
+	public int resolution = 10;
+    Transform[] points;
+
+    void Awake () {
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+        position.y = 0f;
+		position.z = 0f;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++) {
+		    Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+			point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
+        }
+	}
+
+    void Update () {
+	    for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+			Vector3 position = point.localPosition;
+			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            //coefficient for rotation
+            float co = (position.y + 1f) / 2f;
+            //get angle
+            point.localRotation = Quaternion.Euler(0.0f, (co * 360), 0.0f);
+            //Debug.Log(co * 90);
+			point.localPosition = position;
+        }
+	}
+
+}
---
+++ labs/lab10/lab10-readme.txt
@@ -0,0 +1,7 @@
+B481 / Spring 2020, lab 10
+Ethan Anderson (etmander)
+Unity version: 2019.3.1f1
+
+I completed all parts of the lab assignment, including the rotation addition to the tutorial.
+
+I didn't quite understand the wording of the rotation addition, so I did what I thought was correct. Taking the y value of the cube, I add 1 to make it non-negative, then divide by two so that the value now ranges from 0 - 1. I then multiply this value to 360, for degress of rotation. I then apply this to the local rotation of the cube's y direction. 
